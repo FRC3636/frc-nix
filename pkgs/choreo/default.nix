9723cf946845d7807640d223f967dac5b8c4a936
Lewis Arias <lewisgarias@outlook.com>
2024-05-18 15:39:46 -0400

    choreo: 2024.2.1 -> 2024.2.2

--- pkgs/choreo/default.nix
+++ pkgs/choreo/default.nix
@@ -1,34 +1,32 @@
-{ lib, fetchurl, appimageTools, makeDesktopItem }:
+{ lib, fetchurl, appimageTools }:
 
 let
   pname = "choreo";
-  version = "2024.2.1";
+  version = "2024.2.2";
 
-  desktopItem = makeDesktopItem {
-    type = "Application";
-    name = "Choreo";
-    desktopName = "Choreo";
-    comment = "A graphical tool for planning time-optimized trajectories for autonomous mobile robots in the FIRST Robotics Competition.";
-    icon = "choreo";
-    exec = "choreo %u";
+  src = fetchurl {
+    url = "https://github.com/SleipnirGroup/Choreo/releases/download/v${version}/Choreo-v${version}-Linux-x86_64.AppImage";
+    hash = "sha256-z3IdlmKWubuzSnqok7HVXnVlmLAx6JJbdxww9rk93uw=";
   };
 
   icon = fetchurl {
     url = "https://raw.githubusercontent.com/SleipnirGroup/Choreo/v${version}/icon/icon.svg";
     hash = "sha256-HKCzFijS08MKwsMsfTW9ohxWDqyqhRpLhuBjwVWWKPE=";
   };
+
+  appimageContents = appimageTools.extract {
+    inherit pname version src;
+  };
 in
 appimageTools.wrapType2 {
-  inherit pname version;
-
-  src = fetchurl {
-    url = "https://github.com/SleipnirGroup/Choreo/releases/download/v${version}/Choreo-v${version}-Linux-x86_64.AppImage";
-    hash = "sha256-PMBmzLKLsCIbdZjsHUyFjBm7l2nuSGRztA+gB7BLNik=";
-  };
+  inherit pname version src;
 
   extraInstallCommands = ''
-    install -D "${desktopItem}/share/applications/"* -t $out/share/applications/
     install -D ${icon} $out/share/pixmaps/choreo.svg
+    install -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
+
+    substituteInPlace $out/share/applications/${pname}.desktop \
+      --replace "Exec=AppRun" "Exec=${pname}"
   '';
 
   meta = with lib; {
